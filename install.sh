ab081afde03999552cdd615603f59fb4c263dd10
nyxolz <nyxolz@proton.me>
2026-04-10 08:47:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 APP_NAME="Brainrot UI"
-ZIP_NAME="brainrot-ui-1.0.0-arm64-mac.zip"
-DOWNLOAD_URL="https://github.com/nyxolz/BrainrotUI/releases/download/v1.0/$ZIP_NAME"
+
+DMG_NAME="Brainrot.UI-1.0.0-arm64.dmg"
+DOWNLOAD_URL="https://github.com/nyxolz/BrainrotUI/releases/download/v1.0/$DMG_NAME"
 
 echo "------------------------------------------"
 echo "  Installing $APP_NAME by nyxolz"
 echo "------------------------------------------"
+
 
 if [[ $EUID -eq 0 ]]; then
     DEST="/Applications"
@@ -17,25 +19,27 @@ fi
 mkdir -p "$DEST"
 
 echo "-> Downloading from GitHub..."
-curl -L -f -o "/tmp/$ZIP_NAME" "$DOWNLOAD_URL"
+curl -L -f -o "/tmp/$DMG_NAME" "$DOWNLOAD_URL"
 
 if [ $? -ne 0 ]; then
-    echo "Error: Download failed"
+    echo "Error: Download failed. Check if the version and filename are correct."
     exit 1
 fi
 
-if [ -d "$DEST/$APP_NAME.app" ]; then
-    echo "-> Removing old version..."
-    rm -rf "$DEST/$APP_NAME.app"
-fi
+echo "-> Mounting Disk Image..."
 
-echo "-> Extracting to $DEST..."
-unzip -q -o "/tmp/$ZIP_NAME" -d "$DEST"
+MOUNT_DIR=$(hdiutil mount "/tmp/$DMG_NAME" | tail -n1 | perl -nle '/(\/Volumes\/.*)/; print $1')
 
-echo "-> Authorizing app..."
+echo "-> Copying App to $DEST..."
+
+cp -R "$MOUNT_DIR/"*.app "$DEST/$APP_NAME.app"
+
+echo "-> Unmounting and Cleaning up..."
+hdiutil detach "$MOUNT_DIR"
+rm "/tmp/$DMG_NAME"
+
+echo "-> removing quarantine flags"
 xattr -rd com.apple.quarantine "$DEST/$APP_NAME.app" 2>/dev/null
-
-rm "/tmp/$ZIP_NAME"
 
 echo "------------------------------------------"
 echo "Done! You can find $APP_NAME in $DEST"
